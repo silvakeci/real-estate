ca531717878a713b63bb13b1fb234ff107b7fb71
silvakeci <>
2025-05-26 20:27:17 +0200

    setup of application

---
+++ real-estate-app/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <h1>heloo</h1>
+
+    <Button>sub</Button>
+  </div>
+  );
+}
